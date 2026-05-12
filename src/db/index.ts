ee6631527f3c23031ef97be43a7c0fb885127c19
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-05-12 14:01:29 +0200

    build: add db, docker compose

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-
-import * as schema from "./schema.ts";
 import { env } from "#/env/env.ts";
+import * as schema from "./schema.ts";
+
+console.log(env.server);
 
 export const db = drizzle(env.server.DATABASE_URL, { schema });
