79bb4b35087064d40a9492ab8d0511a2a74299fb
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-09-04 16:33:57 +0200

    fix: layout to pos only

--- apps/app/src/features/catalogue/components/snacks-list-item.tsx
+++ apps/app/src/features/catalogue/components/snacks-list-item.tsx
@@ -47,7 +47,7 @@ export function SnacksListItem({
 
   return (
     <motion.li
-      layout
+      layout="position"
       initial={{ opacity: 0, scale: 0.96 }}
       animate={{ opacity: 1, scale: 1 }}
       whileTap={{ scale: 0.96 }}
